8d310af006eb8db2f9b42a34fdb47d3b35a0ff3a
Syd <sydmrmdn@gmail.com>
2019-08-04 20:43:44 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="wallpaper" class="wallpaper is-loaded"></div>
+    <div class="content">
+        <aside class="side">
+            <figure id="picture" class="picture">
+                <div class="picture-shadow"></div>
+                <img id="pictureImage" class="picture-image" src="@/assets/images/alex.jpg" alt="Alex" width="320" height="320">
+            </figure>
+        </aside>
+        <main class="about">
+            <h1 class="name">Hi, I'm Mo Ra</h1>
+            <p class="job">Web Designer / Graphic Designer</p>
+            <hr class="hr">
+            <div class="description">
+                <p>Geek, Designer, Linux Enthusiast, Caffeine Addict and a Serious Eater.</p>
+            </div>
+            <div class="contact">
+                <a class="button" href="mailto:mail@domain.com">
+                    Get in Touch
+                </a>
+                <ul class="social">
+                    <li>
+                        <a href="https://github.com/samoraa">
+                            <i class="mdi mdi-github-circle"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com">
+                            <i class="mdi mdi-twitter"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </main>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@/assets/scripts/animation.js'
+import 'minireset.css'
+import '@/assets/styles/main.css'
+import '@/assets/styles/animation.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
